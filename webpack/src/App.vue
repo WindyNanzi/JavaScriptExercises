467f447fbb0e5801561b51c49a80cb967d27c0fe
hanqiao <hanqiao@lenovocloud.com>
2020-01-15 11:31:33 +0800

    feat: 初步测试Vue是否能打包

---
+++ webpack/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="container"> 
+    <span class="successText">{{text}}</span>
+  </div>  
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      text:''
+    }
+  },
+  created(){
+    this.text = 'webpack 配置 vue 启动成功！'
+  }
+}
+</script>
+
+<style lang="less">
+  body{
+    margin: 0;
+    width: 100%;
+    height: 100%;
+  }
+  #container{
+    width: 100%;
+    height: 100%;
+    .successText{
+      font-weight: bold;
+      font-size: 50px;
+    }
+  }
+</style>
