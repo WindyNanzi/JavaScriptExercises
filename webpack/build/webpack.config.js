2a84663e14feb31f4de85a6f59d21be565e895ac
hanqiao <hanqiao@lenovocloud.com>
2020-01-15 10:22:37 +0800

    feat: 打包 图片，字体，媒体等文件

--- webpack/build/webpack.config.js
+++ webpack/build/webpack.config.js
@@ -32,7 +32,59 @@ module.exports = {
       {
         test:/\.less$/,
         use:['style-loader', 'css-loader', 'postcss-loader','less-loader'] //less文件解析
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i, //图片文件
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                    name: 'img/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/, //媒体文件
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'media/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i, // 字体
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'fonts/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
       }
     ]
   }
 }
+
