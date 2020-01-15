d72e5d63e92b2004e72672d19a43319738e8c280
hanqiao <hanqiao@lenovocloud.com>
2020-01-15 11:31:03 +0800

    fix: clean-webpack-plugin 引入方式更改

--- webpack/build/webpack.config.js
+++ webpack/build/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const vueLoaderPlugin = require('vue-loader/lib/plugin')
 const Webpack = require('webpack')
