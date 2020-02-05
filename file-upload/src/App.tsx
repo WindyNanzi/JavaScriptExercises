47254e4396f7342e001b0aa305f2e5482c66b755
hanqiao <17720074859@163.com>
2020-02-05 16:30:53 +0800

    feat: react+ts+antd 初步模板

---
+++ file-upload/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'antd'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="Content">
+        <Button type='primary'>T-KT</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
