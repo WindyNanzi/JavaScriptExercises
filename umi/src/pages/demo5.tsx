ea957b346cd91ee3fef67eeb2a3233c39e1c4c2c
hanqiao <17720074859@163.com>
2020-03-23 21:52:01 +0800

    fix: 去除无效引用

--- umi/src/pages/demo5.tsx
+++ umi/src/pages/demo5.tsx
@@ -1,4 +1,4 @@
-import React,{ useState, createContext, useContext, useCallback } from 'react'
+import React,{ useState, createContext, useContext } from 'react'
 
 const CountContext = createContext(null)
 
