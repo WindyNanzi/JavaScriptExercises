c434e339037945acd07742483b0464e525e218df
hanqiao <17720074859@163.com>
2020-03-26 22:50:43 +0800

    feat: useReducer +  useContext 结合使用的小例子

--- umi/src/pages/demo7/Button.tsx
+++ umi/src/pages/demo7/Button.tsx
@@ -1,10 +1,14 @@
-import React,{} from 'react'
+import React,{ useContext } from 'react'
+import { ColorContext,UPDATE_COLOR } from './Color'
 
 export default ()=>{
+  const context = useContext(ColorContext)
+  const { dispatch = ()=>{} } = context
+
   return (
     <>
-      <button>黑色</button>
-      <button>黄色</button>
+      <button onClick ={()=> dispatch({ type:UPDATE_COLOR,color:'black' })}>黑色</button>
+      <button onClick ={()=> dispatch({ type:UPDATE_COLOR,color:'yellow'})}>黄色</button>
     </>
   )
 }
--- umi/src/pages/demo7/Color.tsx
+++ umi/src/pages/demo7/Color.tsx
@@ -1,15 +1,30 @@
-import React,{ createContext, Props } from 'react'
-
-declare interface DemoColorIF{
-  color: string
+import React,{ createContext, Props, useReducer } from 'react'
+export declare interface ColorActionType{
+  type:string
+  color?:string
+}
+export declare interface ColorContextIF{
+  color:string
+  dispatch?: React.Dispatch<ColorActionType>
 }
 
-const defaultColor : DemoColorIF = {color: 'red'}
-export const ColorContext = createContext(defaultColor)
+const DEFAULT_COLOR_CONTEXT: ColorContextIF = {color:'red'}
+export const UPDATE_COLOR = 'UPDATE_COLOR'
+export const ColorContext = createContext(DEFAULT_COLOR_CONTEXT)
+
+const colorReducer = (state:string, action:ColorActionType)=>{
+  switch(action.type){
+    case UPDATE_COLOR:
+      return action.color!
+    default:
+      return state
+  }
+}
 
 export const Color = (props:Props<any>) => {
+  const [color, dispatch] = useReducer(colorReducer, 'red')
   return (
-    <ColorContext.Provider value={defaultColor}>
+    <ColorContext.Provider value={{color,dispatch}}>
       {props.children}
     </ColorContext.Provider>
   )
