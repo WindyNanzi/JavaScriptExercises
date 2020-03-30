1d2a1d7d0021852cb9d5c3ac4e860a2686418a56
hanqiao <17720074859@163.com>
2020-03-30 22:12:37 +0800

    feat: 神三元React-Hook小册第一章

---
+++ cloud-music/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { IconStyle  } from './assets/iconfont/iconfont'
+import { GloabStyle } from './style'
+
+
+function App() {
+  return (
+    <div className="App">
+      <GloabStyle></GloabStyle>
+      <IconStyle></IconStyle>
+      <i className='iconfont'>&#xe846;</i>
+    </div>
+  );
+}
+
+export default App;
